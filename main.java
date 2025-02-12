82a0e698f4f06d5c285c758ed361583e06d06109
pujakumari239 <72540991+pujakumari239@users.noreply.github.com>
2025-02-12 15:07:32 +0530

    Initial commit

---
+++ main.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class main
+{
+    public static void main(String[] args) {
+        System.out.print("this is java progem:");
+        System.out.print("output from java program:");
+        System.out.print( new Date());
+    }
+}
