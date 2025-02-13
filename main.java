4cc7ad0127da0483d1b8bccf49d394a4c11773ab
pujakumari239 <72540991+pujakumari239@users.noreply.github.com>
2025-02-13 10:43:35 +0530

    add one more line

--- main.java
+++ main.java
@@ -3,10 +3,10 @@ import java.util.Date;
 public class main
 {
     public static void main(String[] args) {
-        System.out.print("this is java progem:");
-        System.out.print("output from java program:");
-        System.out.print( new Date());
-        System.out.print( "hello");
+        System.out.println("this is java progem:");
+        System.out.println("output from java program:");
+        System.out.println( new Date());
+        System.out.println("hello jenkion");
 
     }
 }
